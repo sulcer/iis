988a020b4c98b4bab2d433605f18b7d1834b8c2e
sulcer <gregor.sulcer@student.um.si>
2024-05-06 18:17:09 +0200

    feat: merge datasets

---
+++ src/data/process_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df_1 = pd.read_csv("data/raw/students_scores.csv")
+df_2 = pd.read_excel('data/raw/students.xlsx')
+
+df = pd.merge(df_1, df_2, left_index=True, right_index=True)
+
+df['AVG_subject'] = (df['STEM_subjects'] + df['H_subjects']) / 2
+
+df.to_csv("data/processed/current_data.csv", index=False)
